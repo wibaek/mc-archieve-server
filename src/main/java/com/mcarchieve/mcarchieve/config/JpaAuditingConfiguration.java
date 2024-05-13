bceadbce516261225c85db12c3f10dd4b4c1196a
devMuromi <devmuromi@gmail.com>
2024-05-13 15:20:16 +0900

    Refactor: @EnableJpaAuditing 분리 테스트시 문제 해결을 위해

---
+++ src/main/java/com/mcarchieve/mcarchieve/config/JpaAuditingConfiguration.java
@@ -0,0 +1,9 @@
+package com.mcarchieve.mcarchieve.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfiguration {
+}
