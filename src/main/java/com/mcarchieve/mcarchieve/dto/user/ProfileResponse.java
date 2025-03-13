05a2f5ecec20bb29529d740a2f8e87655a3c9903
Wibaek Park <devmuromi@gmail.com>
2025-03-14 00:30:30 +0900

    fix: 프로필 이미지 null시 오류 처리

--- src/main/java/com/mcarchieve/mcarchieve/dto/user/ProfileResponse.java
+++ src/main/java/com/mcarchieve/mcarchieve/dto/user/ProfileResponse.java
@@ -1,15 +1,20 @@
 package com.mcarchieve.mcarchieve.dto.user;
 
+import com.mcarchieve.mcarchieve.domain.Image;
 import com.mcarchieve.mcarchieve.domain.user.User;
+
+import java.util.Optional;
 
 public record ProfileResponse(
         String nickname,
         String profileImageUrl
 ) {
+    
     public static ProfileResponse from(User user) {
         return new ProfileResponse(
                 user.getProfile().getNickname(),
-                user.getProfile().getProfileImage().getPath()
-        );
+                Optional.ofNullable(user.getProfile().getProfileImage())
+                        .map(Image::getPath)
+                        .orElse(null));
     }
 }
--- src/main/java/com/mcarchieve/mcarchieve/service/UserService.java
+++ src/main/java/com/mcarchieve/mcarchieve/service/UserService.java
@@ -4,8 +4,6 @@ import com.mcarchieve.mcarchieve.domain.user.Password;
 import com.mcarchieve.mcarchieve.domain.user.User;
 import com.mcarchieve.mcarchieve.dto.user.EmailSignUpRequest;
 import com.mcarchieve.mcarchieve.dto.user.MyInfoResponse;
-import com.mcarchieve.mcarchieve.repository.PasswordRepository;
-import com.mcarchieve.mcarchieve.repository.ProfileRepository;
 import com.mcarchieve.mcarchieve.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -16,10 +14,8 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserService {
 
-    private UserRepository userRepository;
-    private ProfileRepository profileRepository;
-    private PasswordRepository passwordRepository;
-    private PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public MyInfoResponse getUserByEmail(String email) {
         User user = userRepository.findByEmail(email).orElseThrow(() -> new RuntimeException("유저를 찾을 수 없습니다."));
@@ -34,7 +30,6 @@ public class UserService {
 
         Password password = new Password(passwordEncoder.encode(emailSignUpRequest.password()));
         User user = User.createEmailUser(emailSignUpRequest.email(), password, emailSignUpRequest.nickname());
-
         return user;
     }
 }
