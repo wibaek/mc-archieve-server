92a91ac8516b3839d98a8e0716cab947628d5510
Wibaek Park <devmuromi@gmail.com>
2025-03-17 20:20:26 +0900

    refactor: ErrorResponse에서 오류 코드와, http status를 response body에 포함하도록 변경

--- src/main/java/com/mcarchieve/mcarchieve/exception/GlobalExceptionHandler.java
+++ src/main/java/com/mcarchieve/mcarchieve/exception/GlobalExceptionHandler.java
@@ -5,26 +5,25 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @Slf4j
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(CustomException.class)
-    protected ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
-        log.error("CustomException: {}", e.getErrorCode(), e);
-        ErrorResponse errorResponse = new ErrorResponse(e);
+    protected ResponseEntity<ErrorResponse> handleCustomException(CustomException ex) {
+        log.error("CustomException: {}", ex.getErrorCode(), ex);
+        ErrorResponse errorResponse = new ErrorResponse(ex);
 
-        return ResponseEntity.status(e.getErrorCode().getHttpStatus()).body(errorResponse);
+        return ResponseEntity.status(ex.getErrorCode().getHttpStatus()).body(errorResponse);
     }
 
     @ExceptionHandler(Exception.class)
-    protected ResponseEntity<ErrorResponse> handleException(Exception e) {
-        log.error("Unhandled exception occurred: ", e);
+    protected ResponseEntity<ErrorResponse> handleException(Exception ex) {
+        log.error("Unhandled exception occurred: ", ex);
         ErrorResponse errorResponse = new ErrorResponse(
-                "INTERNAL_SERVER_ERROR",
-                "서버 내부 오류가 발생했습니다.",
-                HttpStatus.INTERNAL_SERVER_ERROR.value()
+                "INTERNAL_SERVER_ERROR", "예상하지 못한 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR.value()
         );
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
     }
