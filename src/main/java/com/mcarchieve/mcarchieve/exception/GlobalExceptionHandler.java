bf22217659ab67afab7377b851431a36e81174c4
Wibaek Park <devmuromi@gmail.com>
2025-03-17 20:55:31 +0900

    feat: GlobalExceptionHandler에 handleExceptionInternal() 추가하여 스프링 오류 핸들링
    다만, 생각보다 복잡하여 일괄 처리 해두었는데, 이후에 오류 코드를 정하고 메시지 분기해서 전달해 줘야 할 것으로 보임

--- src/main/java/com/mcarchieve/mcarchieve/exception/GlobalExceptionHandler.java
+++ src/main/java/com/mcarchieve/mcarchieve/exception/GlobalExceptionHandler.java
@@ -1,10 +1,15 @@
 package com.mcarchieve.mcarchieve.exception;
 
+
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @Slf4j
@@ -17,6 +22,18 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         ErrorResponse errorResponse = new ErrorResponse(ex);
 
         return ResponseEntity.status(ex.getErrorCode().getHttpStatus()).body(errorResponse);
+    }
+
+    @Nullable
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, @Nullable Object body, HttpHeaders headers, HttpStatusCode statusCode, WebRequest request) {
+        log.error("Spring Exception: ", ex);
+        // TODO: 스프링 오류 일괄 처리하지 않고, 각각의 오류에 대한 처리를 추가해야 함
+        ErrorResponse errorResponse = new ErrorResponse(
+                "SPRING_EXCEPTION", "스프링 오류가 발생했습니다.", statusCode.value()
+        );
+        return ResponseEntity.status(statusCode).body(errorResponse);
+
     }
 
     @ExceptionHandler(Exception.class)
