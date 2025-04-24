58f7cb9b3da58c8bd9c7858abc36df6dc5229f75
Wibaek Park <devmuromi@gmail.com>
2025-04-24 12:14:44 +0900

    feat: AccessDeniedHandler, AuthenticationEnntryPoint 추가

---
+++ src/main/java/com/mcarchieve/mcarchieve/exception/CustomAccessDeniedHandler.java
@@ -0,0 +1,30 @@
+package com.mcarchieve.mcarchieve.exception;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void handle(HttpServletRequest request,
+                       HttpServletResponse response,
+                       AccessDeniedException accessDeniedException) throws IOException {
+        ErrorResponse errorResponse =
+                new ErrorResponse(new CustomException(ErrorCode.ACCESS_DENIED));
+        response.setStatus(ErrorCode.ACCESS_DENIED.getHttpStatus().value());
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
+    }
+}
